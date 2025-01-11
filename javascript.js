91a14f0f91bfcc5726b67c1d11ccb6a66a692588
Alex Kara <190240100+akarathanassis@users.noreply.github.com>
2025-01-11 16:57:33 +1100

    added operation function to script

--- javascript.js
+++ javascript.js
@@ -17,11 +17,30 @@ function divide(num1, num2) {
     return num1 / num2
 }
 
+function operation(num1, num2, operator) { 
+    switch (operator) { 
+        case "+":  
+            return add(num1, num2); 
+            break; 
+        case "-": 
+            return subtract(num1, num2); 
+            break; 
+        case "*": 
+            return multiply(num1, num2); 
+            break; 
+        case "/": 
+            return divide(num1, num2); 
+            break; 
+        default: 
+            return "Please enter a valid operator (+, -, *, /)"
+    }
+}
+
 let num1 = Number(prompt("Enter Number 1: ")); 
 let num2 = Number(prompt("Enter Number 2: ")); 
+let operator = prompt("Enter an operation (+, -, *, /): "); 
 
-console.log(add(num1, num2)); 
-console.log(subtract(num1, num2)); 
-console.log(divide(num1, num2)); 
+console.log(operator); 
 
+console.log(operation(num1, num2, operator)); 
 
