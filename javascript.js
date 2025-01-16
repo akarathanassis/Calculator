94bed8d3672e1a54060c4cb0c5195cabf069c3fb
Alex Kara <190240100+akarathanassis@users.noreply.github.com>
2025-01-16 12:34:17 +1100

    minor bugfix: output of = typecasted to string
    fixes error when floating point is clicked after
    the equals sign

--- javascript.js
+++ javascript.js
@@ -71,22 +71,22 @@ function evaluateExpression(expression) {
 }
 
 function add(num1, num2) { 
-    return Number(num1) + Number(num2)
+    return String(Number(num1) + Number(num2))
 }
 
 function subtract(num1, num2) { 
-    return num1 - num2
+    return String(num1 - num2)
 }
 
 function multiply(num1, num2) { 
-    return num1 * num2
+    return String(num1 * num2)
 }
 
 function divide(num1, num2) { 
     if (num2 === "0") { 
         return "Undefined"
     }
-    return num1 / num2
+    return String(num1 / num2)
 }
 
 function operation(num1, operator, num2) { 
